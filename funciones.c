93259c21f780828ad1374290c0718b1e75f44f39
Pa Gutri <pagutri@gmail.com>
2017-04-25 17:20:49 -0500

    Aniadi los archivos del proyecto

---
+++ funciones.c
@@ -0,0 +1,131 @@
+/*
+*******FUNCIONES DEL PROYECTO 2*********
+*Salma Patricia Gutiérrez Rivera       *
+*11 de abril de 2017                   *
+****************************************
+Funciones utilizadas en el proyecto.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void imprimir_placa(float **placa, int n) {
+	int i,j;
+	
+	for(i=0;i<n;i++) {
+	    for(j=0;j<n;j++) {
+	        printf("%f\t", placa[i][j]);
+	    }
+	printf("\n");
+	}
+}
+
+/* La siguiente función muestra en pantalla los argumentos que
+necesita el programa si no se especifica un número adecuado de
+ellos en la línea de comandos. */
+void pedir_args(int n) {
+    if(n != 6) {
+	    printf("Necesito cinco argumentos.\n");
+	    printf("El primer argumento es la temperatura del borde izquierdo.\n");
+	    printf("El segundo argumento es la temperatura del borde superior.\n");
+	    printf("El tercer argumento es la temperatura del borde derecho.\n");
+	    printf("El cuarto argumento es la temperatura del borde inferior.\n");
+	    printf("El último argumento es el número de puntos que tendrá nuestra placa.\n");
+	}
+}
+
+/* Esta función arma el estado incial de la placa, asignando ceros a todos los
+puntos interiores y fijando las temperaturas de los bordes. */
+float **iniciar_placa(float t_izq, float t_sup, float t_der, float t_inf, int n)
+{   
+    int i, j; // Contadores
+	float **placa; // Crear puntero
+
+	/* Reservar memoria */ 
+	placa = (float **)malloc(n*sizeof(float*)); 
+	for (i=0;i<n;i++) 
+		placa[i] = (float*)malloc(n*sizeof(float)); 
+
+	/* Inicializar Matriz */
+	for(i=0;i<n;i++) {
+	    for(j=0;j<n;j++) {
+	        if(i==0) placa[i][j] = t_sup;
+	        else if(i==n-1) placa[i][j] = t_inf;
+	        else placa[i][j] = 0.0;
+	    }
+	placa[i][0] = t_izq;
+	placa[i][n-1] = t_der;
+	}
+	
+return placa;
+}
+
+/* Esta función aplica el método de Gauss-Seidel para calcular
+la temperatura de cada punto interior de la placa en función de
+sus vecinos. */
+void aplicar_gauss(float **T, int n)
+{
+    int i, j;
+    for(i=1;i<n-1;i++) {
+        for(j=1;j<n-1;j++) {
+        T[i][j] = (T[i+1][j] + T[i-1][j] + T[i][j+1] + T[i][j-1])/4.0;
+        }
+    }
+}
+
+/* Esta función guarda las temperaturas 'viejas' de la placa para poder
+verificar la condición de paro */
+void copiar_a_vieja(float **nueva, float **vieja, int n)
+{
+    int i, j;
+    for(i=1;i<n-1;i++) {
+        for(j=1;j<n-1;j++) vieja[i][j] = nueva[i][j];
+    }
+}
+
+/* Esta función calcula, para cada celda de la placa, el error porcentual
+relativo a la temperatura actual y la anterior. Cuando el error de todas
+las celdas es menor que cierta epsilon, la función devuelve un 1. */
+int verificar_condicion(float **Tnue, float **Tvie, int n)
+{
+    int i, j;
+    float epsilon = 0.5;
+    float max = 0.0;
+    float error;
+    
+    for(i=1;i<n-1;i++) {
+        for(j=1;j<n-1;j++) {
+        error = fabs((Tnue[i][j] - Tvie[i][j])/Tnue[i][j])*100;
+        if(error>max) max = error;
+        }
+    }
+    
+    if(max<epsilon) return 1; // Verdadero. Se cumple la condición
+    else return 0; // Falso. No se cumple.
+}
+
+/* Esta función crea los archivos con las placas para graficar */
+void crear_archivo(int k, float **placa, int n)
+{
+    char nombre[17];
+    int i,j;
+    
+    sprintf(nombre, "archivos/%i.txt", k);
+    FILE* ptr = fopen(nombre, "a");
+    
+    for(i=0;i<n;i++) {
+	    for(j=0;j<n;j++) {
+	        fprintf(ptr, "%f ", placa[i][j]);
+	    }
+	fprintf(ptr, "\n");
+	}
+    
+    fclose(ptr);
+}
+
+void liberar_memoria(float **ptr, int n)
+{
+    for(int i=0;i<n;i++) free(ptr[i]);
+	free(ptr);
+}
---
+++ pmain.c
@@ -0,0 +1,62 @@
+/*
+*********PROYECTO 2. VERSIÓN 4**********
+*Salma Patricia Gutiérrez Rivera       *
+*17 de abril de 2017                   *
+****************************************
+Función main del Proyecto 2, cuarto bo- 
+rrador.
+Este programa simula la distribución de 
+temperaturas en una placa aislada usando
+el método de Gauss-Seidel.
+Esta versión crea archivos con los cam- 
+bios de temperatura que experimenta la 
+placa.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+void imprimir_placa(float **placa, int n);
+void pedir_args(int n);
+float **iniciar_placa(float t_izq, float t_sup, float t_der, float t_inf, int n);
+void aplicar_gauss(float **T, int n);
+void copiar_a_vieja(float **nueva, float **vieja, int n);
+int verificar_condicion(float **Tnue, float **Tvie, int n);
+void crear_archivo(int k, float **placa, int n);
+void liberar_memoria(float **ptr, int n);
+
+int main(int argc, char *argv[])
+{
+	pedir_args(argc);
+	
+	/* Inicializar variables */
+	int max_iter = 1000;    // Número máximo de iteraciones
+	int iter = 1; // Contador de iteraciones
+	int cumple = 0; // Condición de paro
+	
+	int N = atoi(argv[5]); // Número de puntos de la placa
+	int n = sqrt(N); // Medida del lado de la placa, sin bordes
+	n += 2; // Medida final de la placa
+	
+	/* Temperaturas inciales de la placa */
+	float **placa = iniciar_placa(atof(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), n);
+	float **placaVieja = iniciar_placa(atof(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), n);
+	
+	crear_archivo(0, placa, n);
+	/* Ciclo que implementa el método de Gauss-Seidel */
+	while(iter<=max_iter && cumple==0)
+	{
+	    aplicar_gauss(placa, n);
+	    cumple = verificar_condicion(placa, placaVieja, n);
+	    copiar_a_vieja(placa, placaVieja, n);
+	    crear_archivo(iter, placa, n);
+	    iter++;
+	}
+	
+	liberar_memoria(placa, n);
+	liberar_memoria(placaVieja, n);
+	
+ return 0;
+} 
